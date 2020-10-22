712f36da26c9902e22b6c3300edc54fe3ff8dbe2
Pablo Tato Ramos <pablo.tato@vipera.com>
2020-10-22 12:16:58 +0200

    Done?

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-import bottle, requests
+import bottle, dotenv, requests, os
 
-with open("secret_token.txt") as f:
-    TOKEN = f.read().strip()
 
 def turi(endpoint):
-    return f"https://api.telegram.org/bot{TOKEN}/{endpoint}"
+    token = os.getenv("TELEGRAM_BOT_TOKEN") or "UNDEFINED_BOT_TOKEN"
+    return f"https://api.telegram.org/bot{token}/{endpoint}"
 
 @bottle.route("/")
 def receive_webhook():
     print(bottle.request.json())
+    update = bottle.request.json()
+    if update["message"] != None and update["message"]["text"] != None:
+        text = update["message"]["text"]
+        if text.strip().startswith("https://open.spotify.com/"):
+            new_text = text.split("?")[0].strip()
+            requests.get(turi("sendMessage"), params={
+                "chat_id": update["message"]["chat"]["id"],
+                "text": new_text,
+            })
     return 200
 
 if __name__ == "__main__":
-    host, port = "localhost", 8090
+    dotenv.load_dotenv(verbose=True)
+    host = os.getenv("HOST") or "localhost"
+    port = os.getenv("PORT") or "8090"
+    port = int(port)
     requests.get(turi("setWebhook"), params={
         "url": f"http://{host}:{port}/",
         "certificate": "https://core.telegram.org/bots/api#setwebhook",

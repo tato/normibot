8f71feb9561753d92c205f57d2c267b0355f8329
Pablo Tato Ramos <pabtatoramos@gmail.com>
2020-11-10 11:16:16 +0000

    Log to both a file and stdout
    (hopefully)

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import bottle, dotenv, requests, os, sys, logging
 
-# handlers = [
-#     logging.StreamHandler(stream=sys.stdout),
-#     logging.FileHandler(filename=os.getenv("LOG_FILE") or "/etc/normibot_logs.txt")
-# ]
-# logging.basicConfig(handlers=handlers, level="DEBUG")
 logging.basicConfig(filename=os.getenv("LOG_FILE") or "/etc/normibot_logs.txt", level=logging.DEBUG)
+log = logging.getLogger("normibot")
+log.setLevel(logging.DEBUG)
+fh = logging.FileHandler(os.getenv("LOG_FILE") or "/etc/normibot_logs.txt")
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+log.addHandler(fh)
+log.addHandler(ch)
 
 def turi(endpoint):
     token = os.getenv("TELEGRAM_BOT_TOKEN") or "UNDEFINED_BOT_TOKEN"
@@ -13,29 +19,29 @@ def turi(endpoint):
 
 @bottle.post("/")
 def receive_webhook():
-    logging.info("received webhook, attempting to print json body:")
+    log.info("received webhook, attempting to print json body:")
     try:
-        logging.info(bottle.request.json)
+        log.info(bottle.request.json)
         update = bottle.request.json
         if update["message"] != None and update["message"]["text"] != None:
-            logging.info("apparently there is a message in the update")
-            logging.info("i'm sending something anyway")
+            log.info("apparently there is a message in the update")
+            log.info("i'm sending something anyway")
             requests.get(turi("sendMessage"), params={
                 "chat_id": update["message"]["chat"]["id"],
                 "text": "something",
             })
             text = update["message"]["text"]
             if text.strip().startswith("https://open.spotify.com/"):
-                logging.info("apparently the message is a spotify link")
+                log.info("apparently the message is a spotify link")
                 new_text = text.split("?")[0].strip()
-                logging.info(f"sending '{new_text}'")
+                log.info(f"sending '{new_text}'")
                 requests.get(turi("sendMessage"), params={
                     "chat_id": update["message"]["chat"]["id"],
                     "text": new_text,
                 })
     except Exception as e:
-        logging.error("failed, printing exception")
-        logging.error(e)
+        log.error("failed, printing exception")
+        log.error(e)
     bottle.response.status = 200
 
 if __name__ == "__main__":
@@ -43,13 +49,13 @@ if __name__ == "__main__":
     host = os.getenv("HOST") or "localhost"
     port = os.getenv("PORT") or "8090"
     port = int(port)
-    logging.info(f"started server on {host}:{port}")
+    log.info(f"started server on {host}:{port}")
     requests.get(turi("deleteWebhook"))
     r = requests.get(turi("setWebhook"), params={
         "url": os.getenv("WEBHOOK_URI") or f"http://{host}:{port}/",
         "drop_pending_updates": True,
     })
     r.raise_for_status()
-    logging.info(f"telegram setWebhook: {r.status_code}")
-    logging.info(r.text)
+    log.info(f"telegram setWebhook: {r.status_code}")
+    log.info(r.text)
     bottle.run(host=host, port=port)
